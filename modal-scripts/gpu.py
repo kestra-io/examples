d49d7e4333d865d65a3c8920151df052922b9cd3
Anna Geller <anna.m.geller@gmail.com>
2023-06-14 23:19:32 +0200

    Update gpu.py

--- modal-scripts/gpu.py
+++ modal-scripts/gpu.py
@@ -14,7 +14,9 @@ stub = Stub("gpu-demo")
 def print_gpu_info():
     import torch
 
-    device = torch.cuda.current_device()
+    device_nr = torch.cuda.current_device()
     gpu_count = torch.cuda.device_count()
     device_name = torch.cuda.get_device_name(0)
-    print(f"Device: {device}, GPU count: {gpu_count}, Device name: {device_name}")
+    print(f"Device: {device_nr}, GPU count: {gpu_count}, Device name: {device_name}")
+    print("Memory:", round(torch.cuda.memory_allocated(0) / 1024**3, 1), "GB")
+    print("Memory Cached:   ", round(torch.cuda.memory_cached(0) / 1024**3, 1), "GB")
